55e0435c12493982c6d59ae972361c6db3d10f8f
sherlyjthayil <sherlyjose123@gmail.com>
2023-10-06 15:33:33 +0000

    testing unit test case

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -19,6 +19,7 @@ sap.ui.define([
             onAfterRendering: function(){
                 
                 //testing cicd job
+                //testing unit test case
                 var oModel2 = {
                     editable: false,
                     visibleEnabled:false,
